376d8808fc209659290be8dca303ac4c57820a66
John <johnrdaley22@gmail.com>
2026-06-15 18:25:50 -0400

    Add more tests to meet jacoco coverage threshold

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,12 +1,23 @@
 package com.leszko.calculator;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
-     private Calculator calculator = new Calculator();
+    private Calculator calculator = new Calculator();
 
-     @Test
-     public void testSum() {
-          assertEquals(5, calculator.sum(2, 3));
-     }
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+
+    @Test
+    public void testSumNegative() {
+        assertEquals(-1, calculator.sum(2, -3));
+    }
+
+    @Test
+    public void testSumZero() {
+        assertEquals(0, calculator.sum(0, 0));
+    }
 }
